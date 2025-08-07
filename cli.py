52c9dffa1aec0710547c07ccc8611b31d554c4b2
ShadowPlagueXx <wongchengan@gmail.com>
2025-08-07 16:07:10 +0800

    Basic input in CLI

--- cli.py
+++ cli.py
@@ -1,13 +1,25 @@
 # Action gain = (Total AV in Cycle(s) * Spd + Total Action Advanced) / 10000
-cycles = 0 # default is calculation for 0 cycle
-cycle_av = 150 + cycles * 100
-speed = 0
-ddd_sup = 5 # default is s5
-ddd_procs = 0
-eagle_procs = 0
 
-DDD = 0.16 + 0.02*(ddd_sup - 1)
-EAGLE = 0.25
-advance = DDD*ddd_procs + EAGLE*eagle_procs
+def calculate_actions(speed, cycles=0, ults=0, ddd_procs=0, ddd_sup=5):
+    DDD = 0.16 + 0.02*(ddd_sup - 1)
+    EAGLE = 0.25
+    
+    cycle_av = 150 + cycles * 100
+    advance = EAGLE*ults + DDD*ddd_procs
 
-net_action = (cycle_av*speed + advance) / 10000
+    gain = cycle_av*speed + advance
+    actions = gain / 10000
+    return actions
+
+def main():
+    speed = int(input("Input speed: "))
+    cycles = int(input("Input target cycle count: "))
+    ults = int(input("Input planned ults used (0 if not using eagle): "))
+    ddd_sup = int(input("Input DDD superimpose level: "))
+    ddd_procs = int(input("Input number of DDD procs: "))
+
+    act = calculate_actions(speed, cycles, ults, ddd_procs, ddd_sup)
+    print(act)
+
+if __name__ == "__main__":
+    main()
