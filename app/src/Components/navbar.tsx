f82f6669538f6d4df6cc1e99db80277a9a03b30b
Cheng An Wong <cawong2@sheffield.ac.uk>
2025-10-04 23:21:54 +0100

    Used generative AI to clean up code

--- app/src/Components/navbar.tsx
+++ app/src/Components/navbar.tsx
@@ -2,39 +2,59 @@ function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg">
       <div className="container-fluid">
-        <a
-          className="navbar-brand d-flex align-items-left col-3"
-          href="/index.html"
-        >
-          {/*<img 
-                        src="/Character_Evernight_Icon.webp"
-                        width={50}
-                        height={50}
-                        className="d-inline-block align-top me-2" 
-                    />*/}
+        {/* Left: Brand */}
+        <a className="navbar-brand" href="/index.html">
           HSR Endgame Helper
         </a>
 
-        <ul className="navbar-nav mx-auto align-items-center">
-          <li className="nav-item">
-            <a className="nav-link">AV Calculator</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link">Combat Simulator</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link">Leaderboard</a>
-          </li>
-        </ul>
+        {/* Toggle button for mobile */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarContent"
+          aria-controls="navbarContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-        <ul className="navbar-nav mx-auto align-items-center">
-          <li className="nav-item">
-            <a className="nav-link">Login</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link">Ko-fi</a>
-          </li>
-        </ul>
+        {/* Collapsible content */}
+        <div className="collapse navbar-collapse" id="navbarContent">
+          {/* Center: Main links */}
+          <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                AV Calculator
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Combat Simulator
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Leaderboard
+              </a>
+            </li>
+          </ul>
+
+          {/* Right: Login + Ko-fi */}
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Login
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Ko-fi
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
