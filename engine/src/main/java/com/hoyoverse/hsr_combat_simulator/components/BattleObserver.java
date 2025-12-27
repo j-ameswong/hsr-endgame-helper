0363bb403ce1685c15a393d179bc9be8c4981750
Cheng An Wong <wongchengan@gmail.com>
2025-12-27 02:02:57 +0000

    Removed deprecated old engine

--- engine/src/main/java/com/hoyoverse/hsr_combat_simulator/components/BattleObserver.java
+++
@@ -1,8 +0,0 @@
-package com.hoyoverse.hsr_combat_simulator.components;
-
-import com.hoyoverse.hsr_combat_simulator.model.Action;
-import com.hoyoverse.hsr_combat_simulator.service.CombatEngine;
-
-public interface BattleObserver {
-    void onEvent(Action action, CombatEngine CombatEngine);
-}
--- engine/src/main/java/com/hoyoverse/hsr_combat_simulator/model/enums/LightCone.java
+++
@@ -1,6 +0,0 @@
-package com.hoyoverse.hsr_combat_simulator.model.enums;
-
-public enum LightCone {
-    DANCE_DANCE_DANCE,
-    EVERYTHING_ELSE; // for now
-}
